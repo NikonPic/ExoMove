2057ea8b138c4ca87f14849ece0a817fcaead7ed
NikonPic <nikolas@wilhelm.nu>
2021-09-07 16:59:16 +0200

    simply tell with serial when to start

--- src/Exoskelett_Kraftregelung.ino
+++ src/Exoskelett_Kraftregelung.ino
@@ -288,11 +288,36 @@ void setup()
   openFile_write("log_1.txt");
 
   setupTime = millis();
+
+  getReadyToStart();
+}
+
+void getReadyToStart()
+{
+  Serial.print("Serial available, please type y to start:");
+  int go = 0;
+  while (go == 0)
+  {
+    // read the incoming byte:
+    int incomingByte = Serial.read();
+
+    if (incomingByte != -1)
+    {
+      // say what you got:
+      Serial.print("I received: ");
+      Serial.println(incomingByte);
+      if (incomingByte == 121)
+      {
+        go = 1;
+      }
+    }
+    delay(50);
+  }
 }
 
 void loop()
 {
-  Serial.print(k);
+
   //Positionieren des Aktors nach Loopdurchlaufsnummer
   positionActuator(k);
 
